a3f3c2a2bbf662c1a349fdf3d434caef12950e26
criselgarcia <92629786+criselgarcia@users.noreply.github.com>
2022-07-31 22:52:31 +0800

    Add files via upload

---
+++ Employee_Time_Keeping_System/Employee_Time_Keeping_System/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI_Validation;
+using FILE_STREAM;
+
+
+namespace Employee
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            string userInput = UI_and_Validation.Menu();
+
+            switch (userInput)
+            {
+                case "TI":
+                    UI_and_Validation.CompanyName_Validation();//class.method
+                    File_Stream.inTimeFileStream();//class.method
+                    UI_and_Validation.Details();
+                    break;
+
+                case "TO":
+                    UI_and_Validation.CompanyName_Validation();//class.method
+                    File_Stream.outTimeFileStream();//class.method
+                    UI_and_Validation.Details();
+                    break;
+
+                case "1":
+                    File_Stream.ReadTextinFile();
+                    break;
+                case "2":
+                    UI_and_Validation.CompanyName_Validation();
+                    File_Stream.inTimeUpdateText();
+                    UI_and_Validation.Details();
+                    break;
+                case "3":
+                    UI_and_Validation.CompanyName_Validation();
+                    File_Stream.outTimeUpdateText();
+                    UI_and_Validation.Details();
+                    break;
+                case "4":
+                    File_Stream.DeleteFile();
+                    break;
+                case "5":
+                    File_Stream.ClearTextinFile();
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+
+
+    }
+}
